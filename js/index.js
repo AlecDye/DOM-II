bad0725171db8b29efac95a576be62548e2e9063
Alec Dye <alecdye@gmail.com>
2020-01-14 13:04:38 -0800

    doubleclick and mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+// #1 --- Double Click ---
+const changeImg = document.querySelector('.img-fluid');
+changeImg.addEventListener('dblclick', () => {
+    changeImg.src = 'https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
+});
+// #2 --- Mouse Over ---
+const mouseOver = document.querySelector('.footer');
+mouseOver.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'red';
+    // reset timer
+    setTimeout(() => {
+        event.target.style.backgroundColor = '';
+    }, 500);
+}, false);
+// #3 ---  ---
