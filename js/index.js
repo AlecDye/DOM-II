df06576b035961c67ac214fd09206be4802f8505
Alec Dye <alecdye@gmail.com>
2020-01-14 14:57:10 -0800

    2 to go

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 // Your code goes here
-// #1 --- Double Click ---
-const changeImg = document.querySelector('.img-fluid');
-changeImg.addEventListener('dblclick', () => {
-    changeImg.src = 'https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
+
+// Mouse Events
+// #1 - Click
+const popUp = document.querySelector('.btn');
+popUp.addEventListener('click', event => {
+    alert('WHY ARE YOU PRESSING MY BUTTONS?')
 });
-// #2 --- Mouse Over ---
+// #2 Mouse Enter
+const hoverChange = document.querySelector('.img-content img');
+hoverChange.addEventListener('mouseenter', () => {
+    hoverChange.src = 'https://images.unsplash.com/photo-1561385346-f9218a858231?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80';
+    hoverChange.style.transition = 'transform 0.3s';
+})
+// #3 Mouse Leave
+hoverChange.addEventListener('mouseleave', () => {
+    hoverChange.src = 'https://images.unsplash.com/photo-1563903260263-065d1c592e6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=811&q=80';
+    hoverChange.style.transition = 'transform 0.3s';
+});
+// #4 Mouse Over
 const mouseOver = document.querySelector('.footer');
 mouseOver.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'red';
@@ -13,14 +26,27 @@ mouseOver.addEventListener('mouseover', (event) => {
         event.target.style.backgroundColor = '';
     }, 500);
 }, false);
-// #3 --- Resize ---
-const smallBus = document.querySelector('img');
-window.addEventListener('resize', () => {
-    smallBus.src = 'https://images.unsplash.com/photo-1569135218372-6f7f6f3d7e55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80';
+// #5 Double Click
+const changeImg = document.querySelector('.img-fluid');
+changeImg.addEventListener('dblclick', () => {
+    changeImg.src = 'https://images.unsplash.com/photo-1495904786722-d2b5a19a8535?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=814&q=80'
 });
-// #4 --- Click ---
-const buttonElement = document.querySelector('.btn');
-buttonElement.addEventListener('click', (event) => {
-    alert('You pressed the leftmost button!');
+// #6 Context Menu
+const noContext = document.querySelector('p');
+noContext.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+});
+// #7 Copy
+const colorCopy = document.querySelector('h2');
+colorCopy.addEventListener('copy', (event) => {
+    const selection = document.getSelection();
+    event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
+    event.preventDefault();
 })
-// #5 ---  ---
+// #8
+
+// #10 Resize
+const newBanner = document.querySelector('img');
+window.addEventListener('resize', () => {
+    newBanner.src = 'https://images.unsplash.com/photo-1465172018141-c4e4fa823289?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+});
